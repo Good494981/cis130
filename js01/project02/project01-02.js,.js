b84f51088eb4f586352b45543e68fbfd34fd378a
Good494981 <keyshab75@hotmail.com>
2025-09-23 05:18:53 -0400

    Add files via upload

---
+++ js01/project02/project01-02.js,.js
@@ -0,0 +1,24 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Keysha Bouyer
+    Date:   September 22, 2025
+
+    Filename: project01-02.js
+*/ Purpose Declared Variables
+
+//define variables for service name and service speed
+
+// Declare service names
+let service1Name ="Basic";
+let service2Name ="Express";
+let service3Name ="Extreme";
+let service4Name ="Ultimate";
+
+
+//Declare service speeds
+let service1Speed = "0 Mbps";
+let service2Speed ="100 Mbps";
+let service3Speed ="500 Mbps";
+let service4Speed ="1 Gig";
