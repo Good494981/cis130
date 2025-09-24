d08f497f63f01005c7d87ddcd0e5d0caa94db764
KeyshaBouyer <keyshab75@hotmail.com>
2025-09-24 18:27:22 -0400

    Update project01-04_txt.js

--- js01/project04/project01-04_txt.js
+++ js01/project04/project01-04_txt.js
@@ -2,11 +2,23 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Keysha T. Bouyer
+    Date:   September 22, 2025
 
     Filename: project01-04.js
 */
 
 //define variables for home and work addresses
+javascript
 
+//Home  address variables
+let homeStreet = "1 Main St.";
+let homeCity   =  "Sicilia";
+let homeState  = "MA";
+let homeCode   ="02103";
+
+//Work address variables
+let workStreet = "15 Oak Ln .";
+let workCity   = "Central City";
+let workState  = "MA";
+let workCode   = "02104";
