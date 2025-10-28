8c14b89ea2e8440e95c2e88ab6f8766e25a59333
KeyshaBouyer <keyshab75@hotmail.com>
2025-10-27 21:07:44 -0400

    Add files via upload

--- js01/project02/project01-02_txt.js
+++ js01/project02/project01-02_txt.js
@@ -1,25 +1,11 @@
 /*  JavaScript 7th Edition
     Chapter 1
-    
     Hands-On Project 1-2
 
     Author: Keysha T. Bouyer
-    Date:  September 22, 2025 
+    Date:   September 29, 2025
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
-let serviceName  = "Basic";
-let service2Name = "Express";
-let service3Name = "Extreme";
-
-let service4Name = "Ultimate";
-let service1Speed = "0 Mbps";
-let service2Speed = 100;
-
-let service3Speed = "500 Mbps";
-let service4Speed = "1 Gig";
-
-
-
